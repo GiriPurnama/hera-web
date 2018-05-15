fe69f1823f662b451f714b19dad03475bea744d2
Giri Purnama <hiroyuki.giri@gmail.com>
2018-05-15 16:20:18 +0700

    junk code

--- js/main.js
+++ js/main.js
@@ -192,15 +192,3 @@ $( '#service-carousel .owl-dot' ).on( 'click', function() {
   $( '#service-carousel .owl-dot' ).removeClass( 'active' );
   $(this).addClass( 'active' );
 })
-
-var swiper = new Swiper('.swiper-container', {
-      effect: 'flip',
-      grabCursor: true,
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-});
