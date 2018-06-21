309aee0f4cb7d91d12091b256c08f824a6d5fe44
Giri Purnama <hiroyuki.giri@gmail.com>
2018-06-21 14:06:18 +0700

    Integrasi carousel image

--- index.php
+++ index.php
@@ -95,9 +95,10 @@
             $deksripsi_image = $row['desc_img'];
             $image_home = $row['image_home'];
             $item_class = ($no == 1) ? 'active' : '';
+            $str_home = str_replace("../", "", $image_home);   
           ?>
           <div class="carousel-item <?= $item_class; ?> ">
-            <div class="carousel-background"><img src="img/intro-carousel/1.jpg" alt=""></div>
+            <div class="carousel-background"><img src="<?= $str_home; ?>" alt=""></div>
             <div class="carousel-container">
               <div class="carousel-content">
                 <h2><?= $title_image; ?></h2>
@@ -210,121 +211,8 @@
 
     <!--==========================
       Portfolio Section
-    ============================-->
-    <section id="portfolio"  class="section-bg" >
-      <div class="container">
-
-        <header class="section-header">
-          <h3 class="section-title">Client</h3>
-        </header>
-
-        <div class="row">
-          <div class="col-lg-12">
-            <ul id="portfolio-flters">
-              <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-keuangan">Perusahaan Keuangan</li>
-              <li data-filter=".filter-nonKeuangan">Perusahaan Non Keuangan</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="row portfolio-container">
-
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-nonKeuangan wow fadeInUp" data-wow-delay="0.1s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio/web3.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio/web3.jpg" class="link-preview" data-lightbox="portfolio" data-title="Web 3" title="Preview"><i class="ion ion-eye"></i></a>
-                <!-- <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a> -->
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="#">Web 3</a></h4>
-                <p>Web</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-keuangan wow fadeInUp">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio/card2.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio/card2.jpg" class="link-preview" data-lightbox="portfolio" data-title="Card 2" title="Preview"><i class="ion ion-eye"></i></a>
-                <!-- <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a> -->
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="#">Card 2</a></h4>
-                <p>Card</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-nonKeuangan wow fadeInUp" data-wow-delay="0.1s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio/web2.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio/web2.jpg" class="link-preview" data-lightbox="portfolio" data-title="Web 2" title="Preview"><i class="ion ion-eye"></i></a>
-                <!-- <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a> -->
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="#">Web 2</a></h4>
-                <p>Web</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-keuangan wow fadeInUp">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio/card1.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio/card1.jpg" class="link-preview" data-lightbox="portfolio" data-title="Card 1" title="Preview"><i class="ion ion-eye"></i></a>
-                <!-- <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a> -->
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="#">Card 1</a></h4>
-                <p>Card</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-keuangan wow fadeInUp" data-wow-delay="0.1s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio/card3.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio/card3.jpg" class="link-preview" data-lightbox="portfolio" data-title="Card 3" title="Preview"><i class="ion ion-eye"></i></a>
-                <!-- <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a> -->
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="#">Card 3</a></h4>
-                <p>Card</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-nonKeuangan wow fadeInUp" data-wow-delay="0.2s">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="img/portfolio/web1.jpg" class="img-fluid" alt="">
-                <a href="img/portfolio/web1.jpg" class="link-preview" data-lightbox="portfolio" data-title="Web 1" title="Preview"><i class="ion ion-eye"></i></a>
-                <!-- <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a> -->
-              </figure>
-
-              <div class="portfolio-info">
-                <h4><a href="#">Web 1</a></h4>
-                <p>Web</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- #portfolio -->
+      ============================-->
+    <!-- #portfolio -->
 
     <section id="client" class="wow fadeInUp">
       <div class="container">
