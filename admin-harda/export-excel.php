927393ea60721b7ed46d74c06ab4c40c4170cc42
Giri Purnama <hiroyuki.giri@gmail.com>
2018-08-01 14:44:37 +0700

    fixed report

--- admin-harda/export-excel.php
+++ admin-harda/export-excel.php
@@ -16,14 +16,17 @@ header("Content-Disposition: attachment; filename=pelamar-exportxls-".date("d-m-
   // $nameSql = mysqli_query($db, "SELECT refrensi, MONTHNAME(post_date) AS month, MONTH(post_date) AS tahun_minggu, COUNT(*) AS jumlah FROM recruitment GROUP BY tahun_minggu, refrensi");
   // $nameSql = mysqli_query($db, "SELECT refrensi, MONTHNAME(post_date) AS month, YEARWEEK(post_date) AS tahun_minggu, COUNT(*) AS jumlah FROM recruitment GROUP BY refrensi");
   // $nameSql = mysqli_query($db, "SELECT refrensi, COUNT(*) as jumlah FROM recruitment GROUP BY refrensi");
-  $nameSql = mysqli_query($db, "SELECT WEEK(post_date) AS minggu, refrensi, posisi_rekomendasi, komentar, pengalaman_kerja, nama_lengkap, posisi FROM recruitment WHERE post_date between date_sub(now(),INTERVAL 1 WEEK) and now()  GROUP BY refrensi, posisi")
+  $nameSql = mysqli_query($db, "SELECT WEEK(post_date) AS minggu, refrensi, posisi_rekomendasi, komentar, pengalaman_kerja, nama_lengkap, posisi FROM recruitment WHERE post_date between date_sub(now(),INTERVAL 1 WEEK) and now()  GROUP BY refrensi, posisi");
+  $countPosisi = mysqli_query($db, "SELECT WEEK(post_date) AS minggu, count(*) AS jumlah, refrensi, posisi, posisi_rekomendasi FROM recruitment WHERE post_date between date_sub(now(),INTERVAL 1 WEEK) and now()  GROUP BY posisi ORDER BY refrensi ASC")
   // $jum_pendaftar = mysqli_num_rows($sqlshow);
   // $jum_individu  = mysqli_num_rows($nameSql); 
         
 ?>     
-    <h3>Data Pelamar</h3>
-      
+         
     <table>
+        <tr>
+          <td><h3>Data Pelamar</h3></td>
+        </tr>
         <tr>
           <td width="0px"><b>Tanggal :</b> <?php echo date("d-m-Y") ?></td>
         </tr>
@@ -65,4 +68,51 @@ header("Content-Disposition: attachment; filename=pelamar-exportxls-".date("d-m-
 
    ?>
   </tbody>
-  </table>   
+  </table>
+
+
+
+  
+      
+    <table>
+        <tr>
+          <td><h3>Total Posisi</h3></td>
+        </tr>
+        <tr>
+          <td width="0px"><b>Tanggal :</b> <?php echo date("d-m-Y") ?></td>
+        </tr>
+    </table>    
+        <table border="1">  
+          <thead bgcolor="eeeeee" align="center">
+            <tr bgcolor="eeeeee" >
+             <th>No</ths>
+             <th>Referensi</th>
+             <th>Posisi</th>
+             <th>Jumlah</th>
+            </tr>
+          </thead>
+          <tbody>            
+   
+   <?php
+        
+      $nomor=1;
+      while($rowshow = mysqli_fetch_assoc($countPosisi)){                     
+        $posisi = $rowshow['posisi'];
+        $refrensi = $rowshow['refrensi'];
+        $posisi_rekomendasi = $posisi_rekomendasi ?: $posisi;
+        $jumlah = $rowshow['jumlah'];
+       
+
+          echo '<tr>';
+            echo '<td>'.$nomor.'</td>';
+            echo '<td>'.$refrensi.'</td>';
+            echo '<td>'.$posisi.'</td>';
+            echo '<td>'.$jumlah.'</td>';
+          echo '</tr>';
+          $nomor++;
+      }
+
+
+   ?>
+  </tbody>
+  </table>      
