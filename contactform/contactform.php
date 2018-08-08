5a43a973c50649534822ec50d55488f6992784ab
Giri Purnama <hiroyuki.giri@gmail.com>
2018-08-08 12:19:36 +0700

    balas email

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -4,9 +4,12 @@
 	$subjek = mysqli_real_escape_string($db, trim($_POST['subjek']));
 	$email = mysqli_real_escape_string($db, trim($_POST['email']));
 	$isi_pesan = mysqli_real_escape_string($db, trim($_POST['isi_pesan']));
+	$email_kantor = "recruitment@pthardaesaraksa.com";
+	$header = "PT Harda Esa Raksa"
 
 	$query = mysqli_query($db, "INSERT INTO pesan(nama, subjek, email, isi_pesan, tgl_kirim) values ('$nama','$subjek','$email','$isi_pesan',NOW())");
 	  if ($query) {
+	  		mail($email_kantor,$subjek,$isi_pesan,$header);
 	  		echo "Terima Kasih Masukan anda sudah dikirim";
 	  		
 	  } else {
